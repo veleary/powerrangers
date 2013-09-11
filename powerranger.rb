e34bc7e3001220ab791bfc3add5298fd4594abae
veleary <veleary@gmail.com>
2013-09-11 08:53:42 -0500

    PowerRanger basic methods added

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,32 @@
 class Person
+	attr_accessor :name, :caffeine_level
+	def initialize(name)
+		@name = name
+		@caffeine_level = 1
+	end
+	
+	def run(name,miles)
+		"#{name} just ran #{miles}!"
+	end
+	
+	def scream(name)
+		"#{@name}!"
+	end
+	
+	def drink_coffee(cups_joe)
+		if cups_joe <= 0 
+			puts "Sorry, please put in a number of cups greater than 0."
+		elsif @caffeine_level >= 25 
+			puts "Sorry, you can't have anymore caffeine." 
+		else 
+			@caffeine_level = @caffeine_level + (cups_joe*2)
+			puts "#{@name}'s caffeine level is #{@caffeine_level}."
+		end
+	end
+	
+	def what_is_my_caffeine_level
+		"Your caffeine_level level is #{@caffeine_level}"
+	end
 end
 
 class PowerRanger
